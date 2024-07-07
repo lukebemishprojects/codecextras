f492fef715ba810d53a41cddbc840fdb2bee091a
Luke Bemish <lukebemish@lukebemish.dev>
2024-07-07 12:17:17 -0500

    Format buildSrc

--- buildSrc/src/main/groovy/dev/lukebemish/codecextras/gradle/FormatJmhOutput.groovy
+++ buildSrc/src/main/groovy/dev/lukebemish/codecextras/gradle/FormatJmhOutput.groovy
@@ -8,30 +8,30 @@ import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 abstract class FormatJmhOutput extends DefaultTask {
-	@InputFile
-	abstract RegularFileProperty getJmhResults()
+    @InputFile
+    abstract RegularFileProperty getJmhResults()
 
-	@OutputFile
-	abstract RegularFileProperty getFormattedResults()
+    @OutputFile
+    abstract RegularFileProperty getFormattedResults()
 
-	@TaskAction
-	void format() {
-		// Load JMH JSON results
-		List json = new JsonSlurper().parse(jmhResults.get().getAsFile())
-		List lines = [
-		    "| Benchmark | Mode | Count | Score | | Error | Units |",
-			"| --- | --- | --- | ---: | :---: | :--- | --- |"
-		]
-		for (def result in json) {
-			String benchmark = result.benchmark
-			benchmark = benchmark.split(/\./).dropWhile { it.capitalize() != it}.join('.')
-			String mode = result.mode
-			int count = result.forks * result.measurementIterations
-			double score = result.primaryMetric.score
-			double error = result.primaryMetric.scoreError
-			String units = result.primaryMetric.scoreUnit
-			lines << ("| $benchmark | $mode | $count | ${String.format('%.3f', score)} | ± | ${String.format('%.3f', error)} | $units |" as String)
-		}
-		formattedResults.get().asFile.write(lines.join('\n') + '\n')
-	}
+    @TaskAction
+    void format() {
+        // Load JMH JSON results
+        List json = new JsonSlurper().parse(jmhResults.get().getAsFile())
+        List lines = [
+            "| Benchmark | Mode | Count | Score | | Error | Units |",
+            "| --- | --- | --- | ---: | :---: | :--- | --- |"
+        ]
+        for (def result in json) {
+            String benchmark = result.benchmark
+            benchmark = benchmark.split(/\./).dropWhile { it.capitalize() != it}.join('.')
+            String mode = result.mode
+            int count = result.forks * result.measurementIterations
+            double score = result.primaryMetric.score
+            double error = result.primaryMetric.scoreError
+            String units = result.primaryMetric.scoreUnit
+            lines << ("| $benchmark | $mode | $count | ${String.format('%.3f', score)} | ± | ${String.format('%.3f', error)} | $units |" as String)
+        }
+        formattedResults.get().asFile.write(lines.join('\n') + '\n')
+    }
 }
