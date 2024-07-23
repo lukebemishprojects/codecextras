6a6ad1a4bdf3b1bb0d7f4e1954eebc420c4dffb8
Luke Bemish <lukebemish@lukebemish.dev>
2024-07-23 15:20:51 -0500

    Fix type inference

--- src/main/java/dev/lukebemish/codecextras/types/AppMu.java
+++ src/main/java/dev/lukebemish/codecextras/types/AppMu.java
@@ -3,10 +3,10 @@ package dev.lukebemish.codecextras.types;
 import com.mojang.datafixers.kinds.App;
 import com.mojang.datafixers.kinds.K1;
 
-public record AppMu<Mu extends K1, T>(App<Mu, T> value) implements App<AppMu.Mu, Mu> {
+public record AppMu<M extends K1, T>(App<M, T> value) implements App<AppMu.Mu, M> {
     public static final class Mu implements K1 { private Mu() {} }
 
-    public static <Mu extends K1, T> AppMu<Mu, T> unbox(App<AppMu.Mu, Mu> box) {
-        return (AppMu<Mu, T>) box;
+    public static <M extends K1, T> AppMu<M, T> unbox(App<AppMu.Mu, M> box) {
+        return (AppMu<M, T>) box;
     }
 }
