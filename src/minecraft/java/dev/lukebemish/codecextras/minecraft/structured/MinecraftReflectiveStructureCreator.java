fe3e50ad52019e9908f952d9cea9ed381efc4067
Luke Bemish <lukebemish@lukebemish.dev>
2025-03-03 17:55:59 -0600

    Add StringRepresentable reflective structure creator

--- src/minecraft/java/dev/lukebemish/codecextras/minecraft/structured/MinecraftReflectiveStructureCreator.java
+++ src/minecraft/java/dev/lukebemish/codecextras/minecraft/structured/MinecraftReflectiveStructureCreator.java
@@ -1,14 +1,21 @@
 package dev.lukebemish.codecextras.minecraft.structured;
 
 import com.google.auto.service.AutoService;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import dev.lukebemish.codecextras.structured.Structure;
 import dev.lukebemish.codecextras.structured.reflective.ReflectiveStructureCreator;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.ItemStack;
 
 @AutoService(ReflectiveStructureCreator.class)
@@ -32,6 +39,29 @@ public class MinecraftReflectiveStructureCreator implements ReflectiveStructureC
     @Override
     public List<FlexibleCreator> flexibleCreators() {
         return ImmutableList.<FlexibleCreator>builder()
+            .add(new FlexibleCreator() {
+                @Override
+                public Structure<?> create(Class<?> exact, Function<Type, Structure<?>> creator) {
+                    Supplier<Object[]> values = Suppliers.memoize(() -> {
+                        try {
+                            return (Object[]) exact.getMethod("values").invoke(null);
+                        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                    return Structure.stringRepresentable(values, t -> ((StringRepresentable)t).getSerializedName());
+                }
+
+                @Override
+                public int priority() {
+                    return 10;
+                }
+
+                @Override
+                public boolean supports(Class<?> exact) {
+                    return Enum.class.isAssignableFrom(exact) && StringRepresentable.class.isAssignableFrom(exact);
+                }
+            })
             .build();
     }
 }
