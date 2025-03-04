8ac5f07ec4aa74d8eb47020c1dc86bb633a3e63a
Luke Bemish <lukebemish@lukebemish.dev>
2025-03-04 15:49:46 -0600

    Fix formatting

--- src/minecraft/java/dev/lukebemish/codecextras/stream/structured/StreamCodecInterpreter.java
+++ src/minecraft/java/dev/lukebemish/codecextras/stream/structured/StreamCodecInterpreter.java
@@ -22,13 +22,6 @@ import dev.lukebemish.codecextras.types.Identity;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.DecoderException;
 import io.netty.handler.codec.EncoderException;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.VarInt;
-import net.minecraft.network.codec.ByteBufCodecs;
-import net.minecraft.network.codec.StreamCodec;
-import org.jspecify.annotations.Nullable;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
@@ -40,6 +33,12 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.VarInt;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import org.jspecify.annotations.Nullable;
 
 /**
  * Interprets a {@link Structure} into a {@link StreamCodec} for the same type.
